88fb54ae2b5e22afd44b7f5306a27e292d7533d2
Xinyu Zhu <xyz.hm@hotmail.com>
2025-06-21 19:33:06 +0200

    .

--- g_retriever_m/graph_app_data.py
+++ g_retriever_m/graph_app_data.py
@@ -1,9 +1,9 @@
 import torch
 import importlib
-import networkx as nx
-from torch_geometric.utils import to_networkx
-import json
-from networkx.readwrite import json_graph
+# import networkx as nx
+# from torch_geometric.utils import to_networkx
+# import json
+# from networkx.readwrite import json_graph
 import pandas as pd
 from io import StringIO
 
@@ -56,7 +56,7 @@ def temp_graph_data(subgr, id, list_subgr):
 
 def all_temp_graph(args, list_subgr):
     # obtain list of subgraph data
-    list_subgraphs = [temp_graph_data(subgr, id, list_subgr) for i, subgr in enumerate(list_subgr)]
+    list_subgraphs = [temp_graph_data(subgr, i, list_subgr) for i, subgr in enumerate(list_subgr)]
     # make final dictionary
     final_graph_data = {f"Dataset {args.dataset}": list_subgraphs}
     
